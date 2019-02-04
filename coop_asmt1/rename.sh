3ff1e7439a3596500d64c501dbbd542c9a63b0f4
Tobias He <xtobiashe@gmail.com>
2019-02-04 00:50:00 -0500

    screenshots and echos
    Added meaningful echos and screenshots to show working script

--- coop_asmt1/rename.sh
+++ coop_asmt1/rename.sh
@@ -6,14 +6,18 @@ do
 	if [ "$lines" -eq "0" ]
 	then 
 		rm $file
+        echo removed $file because it was empty
 	elif [ "$lines" -lt "10" ]
 	then
 		mv $file "short_$file"
+        echo renamed $file to short_$file 
 	elif [ "$lines" -ge 10 ] && [ "$lines" -le 20 ]
 	then
 		mv $file "medium_$file"
+        echo renamed $file to medium_$file 
 	elif [ "$lines" -gt 20 ]
 	then
 		mv $file "long_$file"
+        echo renamed $file to long_$file 
 	fi
 done
